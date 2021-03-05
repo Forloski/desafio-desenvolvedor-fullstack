d7875b0bd55c6225e997d50af64393b981953e84
Forloski <f.r.orloski@gmail.com>
2021-03-05 18:36:41 -0300

    Project folder creation.

---
+++ front-end-eqi/src/components/userDetails/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Calculator: React.FC = () => {
+  return <Container />;
+};
+
+export default Calculator;
---
+++ front-end-eqi/src/components/userDetails/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+import { Paper } from '@material-ui/core';
+
+export const Container = styled.div``;
+
+export const StyledPaper = styled(Paper)``;
